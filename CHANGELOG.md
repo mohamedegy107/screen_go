92d0dff74bfe208d22b33283a4209e2c19feee76
Mohamed Elsafty <nesr107@gmail.com>
2024-02-06 03:46:21 +0200

    fix getScreenType and getDeviceType method functions - version 1.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
-## 1.1.0
+## 1.1.1
 
 * fix getScreenType and getDeviceType method functions
--- lib/functions/get_screen_type_func.dart
+++ lib/functions/get_screen_type_func.dart
@@ -1,11 +1,17 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:screen_go/enums/screen_type_enums.dart';
+import 'package:screen_go/models/device_data.dart';
+
+import '../enums/device_type_enums.dart';
 
 DeviceScreenType getScreenType(MediaQueryData mediaQuery) {
   final Orientation orientation = mediaQuery.orientation;
   double width = mediaQuery.size.width;
 
-  if (orientation == Orientation.landscape) {
+  if ((DeviceData.getDeviceType(defaultTargetPlatform) != DeviceType.mobile ||
+          DeviceData.getDeviceType(defaultTargetPlatform) != DeviceType.ios) &&
+      orientation == Orientation.landscape) {
     width = mediaQuery.size.height;
   }
 

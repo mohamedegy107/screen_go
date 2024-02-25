383809e979265be4d8ea760b1e90bd52d103a1d1
Mohamed Elsafty <nesr107@gmail.com>
2024-02-25 22:04:58 +0200

    code refactor version 1.1.5

--- lib/extensions/orienation_type_value.dart
+++ lib/extensions/orienation_type_value.dart
@@ -1,18 +1,15 @@
 import 'package:flutter/material.dart';
-import '../models/device_data.dart';
 
 extension OrientationValueType on dynamic {
   dynamic otv({
-  required BuildContext context,
-    
+    required BuildContext context,
     required dynamic portrait,
     required dynamic landscape,
   }) {
     if (MediaQuery.of(context).orientation == Orientation.portrait) {
       return portrait;
-    } 
-    
-      return landscape;
-    
+    }
+
+    return landscape;
   }
 }
--- lib/extensions/screen_type_value.dart
+++ lib/extensions/screen_type_value.dart
@@ -1,19 +1,21 @@
+import 'package:flutter/material.dart';
+
 import '../enums/screen_type_enums.dart';
-import '../models/device_data.dart';
 
 extension ScreenTypeValue on dynamic {
   dynamic stv({
-  required BuildContext context,
-
+    required BuildContext context,
     required dynamic mobile,
     required dynamic tablet,
     required dynamic desktop,
   }) {
     if (getScreenType(MediaQuery.of(context)) == DeviceScreenType.desktop) {
       return desktop;
-    } else if (getScreenType(MediaQuery.of(context)) == DeviceScreenType.tablet) {
+    } else if (getScreenType(MediaQuery.of(context)) ==
+        DeviceScreenType.tablet) {
       return tablet;
-    } else if ( getScreenType(MediaQuery.of(context))== DeviceScreenType.mobile) {
+    } else if (getScreenType(MediaQuery.of(context)) ==
+        DeviceScreenType.mobile) {
       return mobile;
     } else {
       return this;
